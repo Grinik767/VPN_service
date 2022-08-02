11d27fa7936cf3b3292cb4a7bd9f4e1539b6893a
Grinik767 <grigorikiseleff@yandex.ru>
2022-08-02 21:48:57 +0500

    Add function buttons

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ bot = telebot.TeleBot(os.environ['BOT_TOKEN'])
 @bot.message_handler(commands=["start"])
 def start(message):
     if message.chat.type == 'private' and message.chat.username in os.environ['HAVE_PERMISSION'].split(','):
-        bot.send_message(message.chat.id, "Здравствуйте")
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(types.KeyboardButton("/Add_user"), types.KeyboardButton("/Delete_user"),
+                   types.KeyboardButton("/Get_info_clients"), types.KeyboardButton("/Get_info_server"))
+        bot.send_message(message.chat.id, "Доступ разрешен", reply_markup=markup)
     else:
         bot.send_message(message.chat.id, "Произошла ошибка, попробуйте позже.")
 
