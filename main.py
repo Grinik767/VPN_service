cdac2ce0670e73dcb780e19a84c225f4448544a4
Grinik767 <grigorikiseleff@yandex.ru>
2022-08-02 21:42:59 +0500

    Add user detection

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ bot = telebot.TeleBot(os.environ['BOT_TOKEN'])
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(message.chat.id, "Здравствуйте")
+    if message.chat.type == 'private' and message.chat.username in os.environ['HAVE_PERMISSION'].split(','):
+        bot.send_message(message.chat.id, "Здравствуйте")
+    else:
+        bot.send_message(message.chat.id, "Произошла ошибка, попробуйте позже.")
 
 
 bot.polling(none_stop=True, interval=0)
